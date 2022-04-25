799515485307904fcb535e9c5acc04ef9f6b2c67
SABAH EL MOUHSINI <elmosabah@gmail.com>
2022-04-25 19:15:02 +0200

    filtered journals to display this week only

--- app/controllers/journal_controller.rb
+++ app/controllers/journal_controller.rb
@@ -1,5 +1,17 @@
 class JournalController < ApplicationController
 
+  def index
+
+  end
+
+  def this_week
+    # {:created_at => @selected_date.beginning_of_day..@selected_date.end_of_day}
+    # (:reference_date => 3.months.ago..Time.now)
+    # { :date => 1.week.ago..Time.now }
+    # where("created_at < ?", 2.days.ago)
+    @journals = Journal.where('date < ?', 1.week.ago)
+  end
+
   def show
     @journal = Journal.find(params[:id])
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,17 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # get 'about', to: 'pages#about'
   root to: 'pages#home'
-  get 'journal', to: 'journal#index'
+  # get 'journal', to: 'journal#index'
   get '/journal/show', to: 'journal#show'
+  resources :journals, only: [:index] do
+    collection do
+      get :this_week
+    end
+  end
   # ressource :journal
+  # , only: [:index, :show] do
+  #   resources :products, only: [:index, :show]
+  # end
   # Generic syntax:
   # verb 'path', to: 'controller#action'
 end
