4eae1b4cefd5a1dd2ed6f92880b9d4c0578add8c
SABAH EL MOUHSINI <elmosabah@gmail.com>
2022-04-25 18:12:07 +0200

    syntax error in previous migration => dropped db, created, migrated

--- db/migrate/20220425160501_add_prompt_to_journals.rb
+++ db/migrate/20220425160501_add_prompt_to_journals.rb
@@ -1,7 +1,5 @@
 class AddPromptToJournals < ActiveRecord::Migration[6.1]
-  def change
     def change
       add_column :journals, :prompt, :string
     end
-  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,7 @@ ActiveRecord::Schema.define(version: 2022_04_25_160501) do
     t.date "date"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "prompt"
   end
 
 end
